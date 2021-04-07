2e889f8022e1149b215c9d7a7617d998a646f350
brian-mulei <brianmulei0@gmail.com>
2021-04-07 21:37:07 +0300

    fps controls

---
+++ ShotgunZombies/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+    }
+}
